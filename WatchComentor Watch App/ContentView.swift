42ae7aa5e484b594acb64bc462ac48ec8d511e47
Stv.X <stv.x@icloud.com>
2023-06-12 00:37:35 +0800

    add: watchOS app target

---
+++ WatchComentor Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchComentor Watch App
+//
+//  Created by 徐嗣苗 on 2023/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchComentor Watch App/WatchComentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchComentorApp.swift
+//  WatchComentor Watch App
+//
+//  Created by 徐嗣苗 on 2023/6/12.
+//
+
+import SwiftUI
+
+@main
+struct WatchComentor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
